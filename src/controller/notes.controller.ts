6edaf3ed3955b055d52720ebd3591138adcd3bcb
abelnhm <abelnhm92@gmail.com>
2023-11-21 14:06:50 +0100

    Add error control of update and create in controller

--- src/controller/notes.controller.ts
+++ src/controller/notes.controller.ts
@@ -27,16 +27,24 @@ export class NotesController {
 
   search = (_req: Request, _res: Response) => {};
 
-  async create(req: Request, res: Response) {
-    const result = await this.repo.create(req.body);
-    res.status(201);
-    res.statusMessage = 'Created';
-    res.json(result);
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await this.repo.create(req.body);
+      res.status(201);
+      res.statusMessage = 'Created';
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
   }
 
-  async update(req: Request, res: Response) {
-    const result = await this.repo.update(req.params.id, req.body);
-    res.json(result);
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await this.repo.update(req.params.id, req.body);
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
   }
 
   async delete(req: Request, res: Response, next: NextFunction) {
